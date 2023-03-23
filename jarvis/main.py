9bf8acab7c259533d1ac6851d6d818cd504d328a
Abhimaurya2526 <mauryaabhishek@gmail.com>
2023-03-23 10:13:25 +0530

    update

--- jarvis/main.py
+++ jarvis/main.py
@@ -10,7 +10,7 @@ import operator
 import pywhatkit as kit
 import smtplib
 import json
-import pyautogui
+import pyautogui as p
 import webbrowser
 import pyjokes
 from bs4 import BeautifulSoup
@@ -19,6 +19,8 @@ import subprocess
 import os
 import random
 import re
+import cv2
+import numpy as np
 import requests
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import QTimer, QTime, QDate, Qt
@@ -54,7 +56,7 @@ def speak(audio):
 
 #----------- Function to read -----------
 def pdf_reader():
-    book= open('C:\\Users\\Gaurav Thakur\\Downloads\\final.pdf','rb')
+    book= open('F:\\project\\Group -8 __AI Implemented Virtual Assistant_Chapter-2 -1.pdf','rb')
     pdfReader=PyPDF2.PdfFileReader(book);
     pages = pdfReader.numPages
     speak(f"Total numbers of pages in this book{pages}")
@@ -88,8 +90,8 @@ def My_Location():
 
 #----------- Function to take ScreenShot -----------
 def screenshot():
-    img = pyautogui.screenshot()
-    img.save("E:\\Gaurav\\Projects\\Jarvis\\jarvis_ss\\js.png")
+    img = p.screenshot()
+    img.save("F:\\project\\Assistent_ScreenShot\\js.png")
 
 
 
@@ -104,7 +106,7 @@ def wishMe():
 
     else:
         speak('Good Evening')
-        speak('Pratibha maam and Ranjan Sir. I am your personal assistant, Please tell me how may I help you')
+    speak('Pratibha maam and Ranjan Sir. I am your personal assistant, Please tell me how may I help you')
 
 
 class MainThread(QThread):
@@ -140,6 +142,7 @@ class MainThread(QThread):
          
 # ----------- Function to read -----------
     def TaskExecution(self):
+        
         wishMe()
         while True:
             self.query = self.takeCommand().lower()
@@ -196,7 +199,7 @@ class MainThread(QThread):
 
         # --- Play Music fron local File
             elif 'play music' in self.query:
-                music_dir = 'D:\\Music PlayList'
+                music_dir = 'F:\\project\\Assistent_Music'
                 songs = os.listdir(music_dir)
                 ui.terminalPrint(songs)
                 random = os.startfile(os.path.join(music_dir, songs[1]))
@@ -291,6 +294,76 @@ class MainThread(QThread):
 
 
 # ------------ End Of Program ------------
+
+
+if __name__ == " _main_ ":
+
+    recognizer = cv2.face.LBPHFaceRecognizer_create() # Local Binary Patterns Histograms
+    recognizer.read('jarvis/trainer/trainer.yml')   #load trained model
+    cascadePath = "jarvis\haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascadePath) #initializing haar cascade for object detection approach
+
+    font = cv2.FONT_HERSHEY_SIMPLEX #denotes the font type
+
+
+    id = 2 #number of persons you want to Recognize
+
+
+    names = ['','abhi']  #names, leave first empty bcz counter starts from 0
+
+
+    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW) #cv2.CAP_DSHOW to remove warning
+    cam.set(3, 640) # set video FrameWidht
+    cam.set(4, 480) # set video FrameHeight
+
+    # Define min window size to be recognized as a face
+    minW = 0.1*cam.get(3)
+    minH = 0.1*cam.get(4)
+
+    # flag = True
+
+    while True:
+
+        ret, img =cam.read() #read the frames using the above created object
+
+        converted_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  #The function converts an input image from one color space to another
+
+        faces = faceCascade.detectMultiScale( 
+            converted_image,
+            scaleFactor = 1.2,
+            minNeighbors = 5,
+            minSize = (int(minW), int(minH)),
+        )
+
+        for(x,y,w,h) in faces:
+
+            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2) #used to draw a rectangle on any image
+
+            id, accuracy = recognizer.predict(converted_image[y:y+h,x:x+w]) #to predict on every single image
+
+            # Check if accuracy is less them 100 ==> "0" is perfect match 
+            if (accuracy < 100):
+                id = names[id]
+                accuracy = "  {0}%".format(round(100 - accuracy))
+                TaskExecution()
+
+            else:
+                id = "unknown"
+                accuracy = "  {0}%".format(round(100 - accuracy))
+            
+            cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+            cv2.putText(img, str(accuracy), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        
+        cv2.imshow('camera',img) 
+
+        k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+        if k == 27:
+            break
+
+    # Do a bit of cleanup
+    print("Thanks for using this program, have a good day.")
+    cam.release()
+    cv2.destroyAllWindows()
 
 
 startExecution = MainThread()
@@ -427,3 +500,10 @@ app = QApplication(sys.argv)
 ui = Main()
 ui.show()
 exit(app.exec_())
+
+
+
+
+
+
+
